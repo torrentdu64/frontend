9a951d7b7d4393a7faca097268db644bdc516db4
benoit <torrentdu64@gmail.com>
2025-01-29 13:01:28 +1300

    BUGFIX tailwind compilation

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost', // Set the host to localhost (default)
+    port: 3333, // Change this to your desired port
+  },
 })
